6ab76f3950a2d3e68eaaad5154f9fd789b9bb726
DigiCris <cmarchesetdiii@gmail.com>
2022-08-26 23:28:04 -0300

    reading a value from a smart contract using web3

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Routes } from "react-router-dom";
 import { useWeb3React } from '@web3-react/core';
 import { injected } from '../../config/web3';
+import usePlatziPunks from "../../hooks/usePlatziPunks.js";
 
 const Home = ({connect}) => {
 
   const { active, account, library, connector, activate, deactivate } = useWeb3React();
+
+  const [maxSupply, setMaxSupply] = useState(0);
+
+  const platziPunks = usePlatziPunks();
+
+  async function getMaxSuply()
+  {
+    const result= await platziPunks.methods.maxSupply().call();
+    await setMaxSupply(result);
+  }
 
   async function connect()
   {
@@ -18,6 +29,8 @@ const Home = ({connect}) => {
       <p>Hello world </p>
       <button onClick={connect} >Connect</button>
       <p>{account}</p>
+      <button onClick={getMaxSuply} >Suply</button>
+      <p>{maxSupply}</p>
     </>
   );
 };
